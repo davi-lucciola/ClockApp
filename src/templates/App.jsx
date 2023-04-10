2f546ffc01bb442723c5dbcaf168a12f4b65f464
Davi Lucciola <davilucciola14@gmail.com>
2023-04-10 00:30:43 -0300

    first commit

---
+++ src/templates/App.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import { Cronometro } from "./Cronometro";
+
+
+export class App extends Component {
+  state = {
+    timers: [],
+    current_timer: 0,
+  };
+
+  render() {
+    return (
+      <main>
+        <Cronometro />
+      </main>
+    );
+  }
+}
---
+++ src/templates/Cronometro/index.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export class Cronometro extends Component {
+
+  render() {
+    return (
+      <section className="cronometer">
+        <p> Olá Mundo </p>
+      </section>
+    )
+  }
+}
